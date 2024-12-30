cc8a54428faef03e85fb8f199a52ef9dabac794e
Hakerboi123 <racerburhan@gmail.com>
2024-12-30 04:25:00 +0000

    Initial commit

---
+++ src/main/java/ie/juiceboxstudios/contentcraft/ContentCraft.java
@@ -0,0 +1,14 @@
+package ie.juiceboxstudios.contentcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ContentCraft implements ModInitializer {
+	public static final String MOD_ID = "contentcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {}
+}
